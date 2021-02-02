f1e14fc473ec51a537fa514ac9460511ac8cee43
jiji0612 <36654468+jiji0612@users.noreply.github.com>
2021-02-03 00:03:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -49,7 +49,7 @@ echo "OK";
 function get_reply_message($url, $post_body)
 {
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
