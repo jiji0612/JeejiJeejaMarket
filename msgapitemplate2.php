c890e794ab0f21271ddf95c62d8232bf0251c713
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-03 20:59:44 +0700

    Add

--- msgapitemplate2.php
+++ msgapitemplate2.php
@@ -53,30 +53,6 @@ $msgapitemplate2 = [
 		"size" => "full",
 		"aspectRatio" => "2 =>1"
 	  ],
-	  [
-	  "type" => "bubble",
-	  "body" => [
-			"type" => "box",
-			"layout" => "horizontal",
-			"contents" => [
-			  [
-				"type" => "image",
-				"url" => "https =>//www.linefriends.com/img/img_sec.jpg",
-				"size" => "full",
-				"aspectRatio" => "2 =>1"
-			  ],
-			  [
-				"type" => "filler"
-			  ],
-			  [
-				"type" => "image",
-				"url" => "https =>//www.linefriends.com/img/img_sec.jpg",
-				"size" => "full",
-				"aspectRatio" => "2 =>1"
-			  ]
-			]
-		  ]
-		],
       "body" => [
         "type" => "box",
         "layout" => "vertical",
---
+++ msgapitemplate3.php
@@ -0,0 +1,27 @@
+<?php
+
+$msgapitemplate3 = [
+	  "type" => "bubble",
+	  "body" => [
+			"type" => "box",
+			"layout" => "horizontal",
+			"contents" => [
+			  [
+				"type" => "image",
+				"url" => "https =>//www.linefriends.com/img/img_sec.jpg",
+				"size" => "full",
+				"aspectRatio" => "2 =>1"
+			  ],
+			  [
+				"type" => "filler"
+			  ],
+			  [
+				"type" => "image",
+				"url" => "https =>//www.linefriends.com/img/img_sec.jpg",
+				"size" => "full",
+				"aspectRatio" => "2 =>1"
+			  ]
+			]
+		  ]
+		];
+?>
