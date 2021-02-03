a502709bb2b535801da429080b371b45538a41db
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-03 20:22:57 +0700

    Change

--- msgapitemplate2.php
+++ msgapitemplate2.php
@@ -14,9 +14,9 @@ $msgapitemplate2 = [
   ],
   "hero" => [
     "type" => "image",
-    "url" => "https =>//www.linefriends.com/img/img_sec.jpg",
+    "url" => "https://www.linefriends.com/img/img_sec.jpg",
     "size" => "full",
-    "aspectRatio" => "2 =>1"
+    "aspectRatio" => "2:1"
   ],
   "body" => [
     "type" => "box",
