c1c8b300ea16e995f64fdba8771981799e1b25a6
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-03 20:42:37 +0700

    Update msgapitemplate2.php

--- msgapitemplate2.php
+++ msgapitemplate2.php
@@ -1,47 +1,121 @@
 <?php
 
-$msgapitemplate2 = [
-  "type" => "bubble",
-  "header" => [
-    "type" => "box",
-    "layout" => "vertical",
+$msgapitemplate = [
+    "type" => "flex",
+    "altText" => "Flex Message",
     "contents" => [
-      [
-        "type" => "text",
-        "text" => "header"
-      ]
-    ]
-  ],
-  "hero" => [
-    "type" => "image",
-    "url" => "https://www.linefriends.com/img/img_sec.jpg",
-    "size" => "full",
-    "aspectRatio" => "2:1"
-  ],
-  "body" => [
-    "type" => "box",
-    "layout" => "vertical",
-    "contents" => [
-      [
-        "type" => "text",
-        "text" => "body"
+      "type" => "bubble",
+      "direction" => "ltr",
+      "header" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "รายการสินค้า",
+            "size" => "lg",
+            "align" => "start",
+            "weight" => "bold",
+            "color" => "#009813"
+          ],
+          [
+            "type" => "text",
+            "text" => "ขนมขบเคี้ยว",
+            "margin" => "lg",
+            "size" => "lg",
+            "color" => "#000000"
+          ]
+        ]
       ],
-	  [
-        "type" => "text",
-        "text" => "body"
+	  "hero" => [
+	  "contents" => [
+	      [
+			"type" => "image",
+			"url" => "https://www.linefriends.com/img/img_sec.jpg",
+			"size" => "full",
+			"aspectRatio" => "2:1"
+		  ],
+		  [
+			"type": "filler"
+          ],
+		  [
+			"type" => "image",
+			"url" => "https://www.linefriends.com/img/img_sec.jpg",
+			"size" => "full",
+			"aspectRatio" => "2:1"
+		  ]
+		]
+	  ],
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "separator",
+            "color" => "#C3C3C3"
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "Merchant",
+                "align" => "start",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "BTS 01",
+                "align" => "end",
+                "color" => "#000000"
+              ]
+            ]
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "New balance",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "฿ 45.57",
+                "align" => "end"
+              ]
+            ]
+          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ]
+        ]
+      ],
+      "footer" => [
+        "type" => "box",
+        "layout" => "horizontal",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "View Details",
+            "size" => "lg",
+            "align" => "start",
+            "color" => "#0084B6",
+            "action" => [
+              "type" => "uri",
+              "label" => "View Details",
+              "uri" => "https://google.co.th/"
+            ]
+          ]
+        ]
       ]
     ]
-  ],
-  "footer" => [
-    "type" => "box",
-    "layout" => "vertical",
-    "contents" => [
-      [
-        "type" => "text",
-        "text" => "footer"
-      ]
-    ]
-  ]
-];
-
+  ];
+  
   ?>
