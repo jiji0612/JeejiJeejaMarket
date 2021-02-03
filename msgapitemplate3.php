60ba837f1c3c53d40a3e487278451a00b1e671f4
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-03 21:03:53 +0700

    Update msgapitemplate3.php

--- msgapitemplate3.php
+++ msgapitemplate3.php
@@ -8,18 +8,14 @@ $msgapitemplate3 = [
 			"contents" => [
 			  [
 				"type" => "image",
-				"url" => "https://www.linefriends.com/img/img_sec.jpg",
-				"size" => "full",
-				"aspectRatio" => "2:1"
+				"url" => "https://www.linefriends.com/img/img_sec.jpg"
 			  ],
 			  [
 				"type" => "filler"
 			  ],
 			  [
 				"type" => "image",
-				"url" => "https://www.linefriends.com/img/img_sec.jpg",
-				"size" => "full",
-				"aspectRatio" => "2:1"
+				"url" => "https://www.linefriends.com/img/img_sec.jpg"
 			  ]
 			]
 		  ]
