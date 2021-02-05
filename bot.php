16112c8233fe477c67971cff2384aa677e345be2
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-06 00:24:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -55,7 +55,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 		//Debug Code
 		$data = [
 			'replyToken' => $reply_token,
-			'messages' => [['type' => 'text', 'text' => '"'.$send_result.'"' ]]
+			'messages' => [['type' => 'text', 'text' => $send_result ]]
 		];
 		$post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
