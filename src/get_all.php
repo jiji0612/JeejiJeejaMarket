af3c68bc84df244f46af25189f72bec22557db85
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 00:48:06 +0700

    Update get_all.php

--- src/get_all.php
+++ src/get_all.php
@@ -42,7 +42,7 @@ function get_all($projectId)
     ]);
     # [START fs_get_all]
     $usersRef = $db->collection('Order');
-    $snapshot = $usersRef->documents();
+    $snapshot = $usersRef->documents('viOlK4Zr8Kejv05WmC0s');
     foreach ($snapshot as $user) {
         printf('OrderNo: %s' . PHP_EOL, $user->id());
         printf('Items: %s' . PHP_EOL, $user['items']);
