a3050b67a9d51ced1e036e841c95693144fe9714
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 15:04:43 +0700

    Update testconn2.php

--- dbconn/testconn2.php
+++ dbconn/testconn2.php
@@ -1,12 +1,11 @@
 <?php
     echo "Starting";
-    $url = parse_url (getenv ("mysql://ba7f82c856e7b5:9090f5ee@us-cdbr-east-03.cleardb.com/heroku_565ce00c19449b1?reconnect=true"));
 
 	$server = "us-cdbr-east-03.cleardb.com";
 	$username = "ba7f82c856e7b5";
 	$password = "9090f5ee";
-	$db = "heroku_565ce00c19449b1";
-	$link = mysqli_connect ($server, $username, $password, $db);
+	//$db = "heroku_565ce00c19449b1";
+	$link = mysqli_connect ($server, $username, $password);
 	$result = mysqli_query ($link,"select * from vi_member_order");
 	echo $result;
 
