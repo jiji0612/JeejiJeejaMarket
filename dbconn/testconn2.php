9f35c34ec3660a0451ad2d3409a0612fe74b086e
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 19:10:07 +0700

    Update testconn2.php

--- dbconn/testconn2.php
+++ dbconn/testconn2.php
@@ -1,16 +1,24 @@
-
-<?php
-	$url = parse_url (getenv ("CLEARDB_DATABASE_URL"));
-
-	$server = $url ["host"];
-	$username = $url ["user"];
-	$password = $url ["pass"];
-	$db = substr ($url ["path"], 1);
-	$link = mysqli_connect ($server, $username, $password, $db);
-	mysqli_query("SET NAMES TIS620");
-	$result = mysqli_query ($link,"select * from vi_member_order");
-
-	while ($member = mysqli_fetch_array ($result)) {
-	echo $member ['memberid'] . ":" . $member ['addr'] . "<br>";
-	}
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<body>
+<?
+ 
+    $message = "รับทำเว็บ รับเขียนเว็บ เรียนเขียนโปรแกรม";
+     
+    $tis620 = iconv("utf-8", "tis-620", $message );
+    $utf8 = iconv("tis-620", "utf-8", $tis620 );
+     
+    echo "Page charset=utf-8";
+    echo "<br/>";
+    echo "Convert from UTF-8 to TIS-620 = ".$tis620;
+    echo "<br/>";
+    echo "Convert from TIS-620 to UTF-8 = ".$utf8;
+ 
 ?>
+</body>
+</html>
