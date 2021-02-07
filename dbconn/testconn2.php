18c159d1da46c2329d0f6aafb7ef4264525be1e1
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 15:09:49 +0700

    Update testconn2.php

--- dbconn/testconn2.php
+++ dbconn/testconn2.php
@@ -1,15 +1,16 @@
 <?php
     echo "Starting";
 
-	$server = "us-cdbr-east-03.cleardb.com";
-	$username = "ba7f82c856e7b5";
-	$password = "9090f5ee";
-	//$db = "heroku_565ce00c19449b1";
-	$link = mysqli_connect ($server, $username, $password);
-	$result = mysqli_query ($link,"select * from vi_member_order");
-	echo $result;
-
-	while ($user = mysqli_fetch_array ($result)) {
-	echo $user['memberid'] . ":" . $user['addr'] . "<br>" ;
+	$mysqli = new mysqli("us-cdbr-east-03.cleardb.com", "ba7f82c856e7b5", "9090f5ee", "heroku_565ce00c19449b1");
+	if ($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
+	echo $mysqli->host_info . "\n";
+
+	$mysqli = new mysqli("127.0.0.1", "ba7f82c856e7b5", "9090f5ee", "heroku_565ce00c19449b1", 3306);
+	if ($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+
+	echo $mysqli->host_info . "\n";
 ?>
