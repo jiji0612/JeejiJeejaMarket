d7893242166f733b82af594396122e7192e06d2f
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 18:45:25 +0700

    Update addorder.php

--- dbconn/addorder.php
+++ dbconn/addorder.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	/*** Update Member ***/
     $strSQL = "INSERT INTO member (memberid,membername) ";
     $strSQL .= "SELECT '".$uid."','".$user_name."' FROM DUAL WHERE NOT EXISTS (SELECT memberid FROM member WHERE memberid = '".$uid."') ";
-    $strSQL .= "UPDATE member set remark = '".$ordersubmit."' WHERE memberid = '".$uid."'";
+    //$strSQL .= "UPDATE member set remark = '".$ordersubmit."' WHERE memberid = '".$uid."'";
     if (mysqli_query($conn, $strSQL)) {
         echo "successfully";
     } else {
