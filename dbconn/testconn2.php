4c7186262d81155acfbf82edbadaaed91995c048
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 14:34:26 +0700

    Create testconn2.php

---
+++ dbconn/testconn2.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>PHP Test MySql Connection</title>
+<meta http-equiv=Content-Type content="text/html; charset=tis-620">
+</head>
+<body>
+
+	<table width="498" border="1">
+	<tr>
+	<th width="87"> <div align="center">MemberID </div></th>
+	<th width="145"> <div align="center">Address </div></th>
+	<th width="244"> <div align="center">Order No </div></th>
+    <th width="244"> <div align="center">Items Name </div></th>
+    <th width="244"> <div align="center">Quantity </div></th>
+    <th width="244"> <div align="center">Price </div></th>
+	</tr>
+		<tr>
+		<td><div align="center"></div></td>
+		<td></td>
+		<td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+	</tr>		
+	<form name="frmMain" method="post" action="?Action=Save">
+		<tr>
+		  <td>
+	      </td>
+		  <td><input name="txtMember" type="text" id="txtMember"></td>
+		  <td><input name="txtAddr" type="text" id="txtAddr">
+	      <input name="btnSubmit" type="submit" id="btnSubmit" value="Submit"></td>
+	    </tr>
+</form>	  
+
+</table>
+
+</body>
+</html> 
