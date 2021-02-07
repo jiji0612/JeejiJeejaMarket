0f3631bd4da85cc9544d537f1d230e2630428155
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 15:02:54 +0700

    Update testconn2.php

--- dbconn/testconn2.php
+++ dbconn/testconn2.php
@@ -7,11 +7,10 @@
 	$password = "9090f5ee";
 	$db = "heroku_565ce00c19449b1";
 	$link = mysqli_connect ($server, $username, $password, $db);
-	echo $server.$username.$password.$db;
-
 	$result = mysqli_query ($link,"select * from vi_member_order");
+	echo $result;
 
 	while ($user = mysqli_fetch_array ($result)) {
-	echo $user['memberid'].":".$user['addr']."<br>" ;
+	echo $user['memberid'] . ":" . $user['addr'] . "<br>" ;
 	}
 ?>
