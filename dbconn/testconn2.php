d7ec6f0a3474ef1ab21687c4cfd94dc2ea2127d8
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 15:12:59 +0700

    Update testconn2.php

--- dbconn/testconn2.php
+++ dbconn/testconn2.php
@@ -1,9 +1,4 @@
 <?php
-	$mysqli = new mysqli("us-cdbr-east-03.cleardb.com", "ba7f82c856e7b5", "9090f5ee", "heroku_565ce00c19449b1");
-	if ($mysqli->connect_errno) {
-		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-	}
-	echo $mysqli->host_info . "\n";
 
 	$mysqli = new mysqli("127.0.0.1", "ba7f82c856e7b5", "9090f5ee", "heroku_565ce00c19449b1", 3306);
 	if ($mysqli->connect_errno) {
