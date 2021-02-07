dcfab49fe3825ad40167605a01dcf6090fe00818
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 16:46:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -58,10 +58,10 @@ if ( sizeof($request_array['events']) > 0 ) {
 			$API_GET = "https://api.line.me/v2/bot/profile/" . $uid;
 			$GET_HEADER = array('Authorization: Bearer ' . $ACCESS_TOKEN);
 			$get_user = get_reply_message($API_GET, $GET_HEADER);
-			//$json_a = json_decode($get_user, true);
+			$json_a = json_decode($get_user, true);
 			$data = [
 				'replyToken' => $reply_token,
-				'messages' => [['type' => 'text', 'text' => '"'.$get_user.'"' ]]
+				'messages' => [['type' => 'text', 'text' => '"'.$json_a["displayName"].'"' ]]
 			];
 			$post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 		}
