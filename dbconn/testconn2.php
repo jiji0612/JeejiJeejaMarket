5aff3ad18b0dfc6bff026b43e9d9ecb96331f2f0
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-07 15:00:49 +0700

    Update testconn2.php

--- dbconn/testconn2.php
+++ dbconn/testconn2.php
@@ -1,11 +1,11 @@
 <?php
     echo "Starting";
     $url = parse_url (getenv ("mysql://ba7f82c856e7b5:9090f5ee@us-cdbr-east-03.cleardb.com/heroku_565ce00c19449b1?reconnect=true"));
-	echo "Starting 2";
-	$server = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$db = substr ($url["path"], 1);
+
+	$server = "us-cdbr-east-03.cleardb.com/";
+	$username = "ba7f82c856e7b5";
+	$password = "9090f5ee";
+	$db = "heroku_565ce00c19449b1";
 	$link = mysqli_connect ($server, $username, $password, $db);
 	echo $server.$username.$password.$db;
 
