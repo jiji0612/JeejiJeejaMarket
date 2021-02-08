3e6bd3f7b59a2050da6060ccee4f5c8849ec690d
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-08 22:28:18 +0700

    Update summary_order.php

--- dbconn/summary_order.php
+++ dbconn/summary_order.php
@@ -138,6 +138,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 }
                             ]
                             },
+                            {
                             "type": "box",
                             "layout": "vertical",
                             "spacing": "sm",
