c12e0c9ac830d3f3172dd74a8a298008fdc44654
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-08 22:13:16 +0700

    Update summary_order.php

--- dbconn/summary_order.php
+++ dbconn/summary_order.php
@@ -15,6 +15,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $objQuery = mysqli_query ($conn,"select * from vi_member_order where memberid = '".$uid."'");
     $arr_order_lst = '';
+    $sum_qty = '0';
+    $sum_price = '0';
 
     while($objResult = mysqli_fetch_array($objQuery))
     {
@@ -41,170 +43,139 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ';
     }
 
+    //Summary
+    $objQuery = mysqli_query ($conn,"select sum(total_qty) as total_qty,sum(total_price) as total_price from vi_member_order where memberid = '".$uid."'");
+    while($objResult = mysqli_fetch_array($objQuery))
+    {
+        $sum_qty = $objResult["total_qty"];
+        $sum_price = $objResult["total_price"];
+    }
 
-        $jsonobj = '{
-            "replyToken": "",
-            "messages": [
-                {
-                    "type": "flex",
-                    "altText": "Hello Flex Message",
-                    "contents": {
-                        "type": "bubble",
-                        "body": {
+    $jsonobj = '{
+        "replyToken": "",
+        "messages": [
+            {
+                "type": "flex",
+                "altText": "Hello Flex Message",
+                "contents": {
+                    "type": "bubble",
+                    "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                        {
+                        "type": "text",
+                        "text": "ร้าน จ้ากะจี้ เฟส1",
+                        "weight": "bold",
+                        "color": "#1DB446",
+                        "size": "sm"
+                        },
+                        {
+                        "type": "text",
+                        "text": "รายการสั่งซื้อ",
+                        "weight": "bold",
+                        "size": "xxl",
+                        "margin": "md"
+                        },
+                        {
+                        "type": "text",
+                        "text": "ตะกร้า",
+                        "size": "xs",
+                        "color": "#aaaaaa",
+                        "wrap": true
+                        },
+                        {
+                        "type": "separator",
+                        "margin": "xxl"
+                        },
+                        {
                         "type": "box",
                         "layout": "vertical",
-                        "contents": [
-                            {
-                            "type": "text",
-                            "text": "ร้าน จ้ากะจี้ เฟส1",
-                            "weight": "bold",
-                            "color": "#1DB446",
-                            "size": "sm"
-                            },
-                            {
-                            "type": "text",
-                            "text": "รายการสั่งซื้อ",
-                            "weight": "bold",
-                            "size": "xxl",
-                            "margin": "md"
-                            },
-                            {
-                            "type": "text",
-                            "text": "ตะกร้า",
-                            "size": "xs",
-                            "color": "#aaaaaa",
-                            "wrap": true
-                            },
-                            {
-                            "type": "separator",
-                            "margin": "xxl"
-                            },
-                            {
-                            "type": "box",
-                            "layout": "vertical",
-                            "margin": "xxl",
-                            "spacing": "sm",
-                            "contents": ['
-                                .$arr_order_lst.
-                                '{
-                                "type": "separator",
-                                "margin": "xxl"
-                                },
-                                {
-                                "type": "box",
-                                "layout": "horizontal",
-                                "margin": "xxl",
-                                "contents": [
-                                    {
-                                    "type": "text",
-                                    "text": "จำนวน",
-                                    "size": "sm",
-                                    "color": "#555555"
-                                    },
-                                    {
-                                    "type": "text",
-                                    "text": "3",
-                                    "size": "sm",
-                                    "color": "#111111",
-                                    "align": "end"
-                                    }
-                                ]
-                                },
-                                {
-                                "type": "box",
-                                "layout": "horizontal",
-                                "contents": [
-                                    {
-                                    "type": "text",
-                                    "text": "ยอดรวมสินค้า",
-                                    "size": "sm",
-                                    "color": "#555555"
-                                    },
-                                    {
-                                    "type": "text",
-                                    "text": "฿10",
-                                    "size": "sm",
-                                    "color": "#111111",
-                                    "align": "end"
-                                    }
-                                ]
-                                },
-                                {
-                                "type": "box",
-                                "layout": "horizontal",
-                                "contents": [
-                                    {
-                                    "type": "text",
-                                    "text": "ค่าจัดส่ง",
-                                    "size": "sm",
-                                    "color": "#555555"
-                                    },
-                                    {
-                                    "type": "text",
-                                    "text": "฿0",
-                                    "size": "sm",
-                                    "color": "#111111",
-                                    "align": "end"
-                                    }
-                                ]
-                                },
-                                {
-                                "type": "box",
-                                "layout": "horizontal",
-                                "contents": [
-                                    {
-                                    "type": "text",
-                                    "text": "ทั้งหมด",
-                                    "size": "sm",
-                                    "color": "#555555"
-                                    },
-                                    {
-                                    "type": "text",
-                                    "text": "฿10",
-                                    "size": "sm",
-                                    "color": "#111111",
-                                    "align": "end"
-                                    }
-                                ]
-                                }
-                            ]
-                            },
-                            {
+                        "margin": "xxl",
+                        "spacing": "sm",
+                        "contents": ['
+                            .$arr_order_lst.
+                            '{
                             "type": "separator",
                             "margin": "xxl"
                             },
                             {
                             "type": "box",
                             "layout": "horizontal",
-                            "margin": "md",
+                            "margin": "xxl",
                             "contents": [
                                 {
                                 "type": "text",
-                                "text": "การจ่ายเงิน",
-                                "size": "xs",
-                                "color": "#aaaaaa",
-                                "flex": 0
+                                "text": "จำนวนทั้งหมด",
+                                "size": "sm",
+                                "color": "#555555"
                                 },
                                 {
                                 "type": "text",
-                                "text": "ปลายทาง",
-                                "color": "#aaaaaa",
-                                "size": "xs",
+                                "text": "'.$sum_qty.'",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                                }
+                            ]
+                            },
+                            {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                                {
+                                "type": "text",
+                                "text": "ราคาทั้งหมด",
+                                "size": "sm",
+                                "color": "#555555"
+                                },
+                                {
+                                "type": "text",
+                                "text": "'.$sum_price.'฿",
+                                "size": "sm",
+                                "color": "#111111",
                                 "align": "end"
                                 }
                             ]
                             }
                         ]
                         },
-                        "styles": {
-                        "footer": {
-                            "separator": true
+                        {
+                        "type": "separator",
+                        "margin": "xxl"
+                        },
+                        {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "margin": "md",
+                        "contents": [
+                            {
+                            "type": "text",
+                            "text": "การจ่ายเงิน",
+                            "size": "xs",
+                            "color": "#aaaaaa",
+                            "flex": 0
+                            },
+                            {
+                            "type": "text",
+                            "text": "ปลายทาง",
+                            "color": "#aaaaaa",
+                            "size": "xs",
+                            "align": "end"
+                            }
+                        ]
                         }
-                        }
+                    ]
+                    },
+                    "styles": {
+                    "footer": {
+                        "separator": true
+                    }
                     }
                 }
-            ]
-        }';
+            }
+        ]
+    }';
 
     //var_dump(json_decode($jsonobj, true));
     mysqli_close($conn);
