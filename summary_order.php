98b01c44948a0dd218dde2f3d9b883b99a937598
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-08 21:13:15 +0700

    Create summary_order.php

---
+++ summary_order.php
@@ -0,0 +1,226 @@
+<?php
+$jsonobj = '{
+    "replyToken": "",
+    "messages": [
+        {
+            "type": "flex",
+            "altText": "Hello Flex Message",
+            "contents": {
+                "type": "bubble",
+                "body": {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "ร้าน จ้ากะจี้ เฟส1",
+                      "weight": "bold",
+                      "color": "#1DB446",
+                      "size": "sm"
+                    },
+                    {
+                      "type": "text",
+                      "text": "รายการสั่งซื้อ",
+                      "weight": "bold",
+                      "size": "xxl",
+                      "margin": "md"
+                    },
+                    {
+                      "type": "text",
+                      "text": "ตะกร้า",
+                      "size": "xs",
+                      "color": "#aaaaaa",
+                      "wrap": true
+                    },
+                    {
+                      "type": "separator",
+                      "margin": "xxl"
+                    },
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "margin": "xxl",
+                      "spacing": "sm",
+                      "contents": [
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Energy Drink",
+                              "size": "sm",
+                              "color": "#555555",
+                              "flex": 0
+                            },
+                            {
+                              "type": "text",
+                              "text": "$2.99",
+                              "size": "sm",
+                              "color": "#111111",
+                              "align": "end"
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Chewing Gum",
+                              "size": "sm",
+                              "color": "#555555",
+                              "flex": 0
+                            },
+                            {
+                              "type": "text",
+                              "text": "$0.99",
+                              "size": "sm",
+                              "color": "#111111",
+                              "align": "end"
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Bottled Water",
+                              "size": "sm",
+                              "color": "#555555",
+                              "flex": 0
+                            },
+                            {
+                              "type": "text",
+                              "text": "$3.33",
+                              "size": "sm",
+                              "color": "#111111",
+                              "align": "end"
+                            }
+                          ]
+                        },
+                        {
+                          "type": "separator",
+                          "margin": "xxl"
+                        },
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "margin": "xxl",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "จำนวน",
+                              "size": "sm",
+                              "color": "#555555"
+                            },
+                            {
+                              "type": "text",
+                              "text": "3",
+                              "size": "sm",
+                              "color": "#111111",
+                              "align": "end"
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "ยอดรวมสินค้า",
+                              "size": "sm",
+                              "color": "#555555"
+                            },
+                            {
+                              "type": "text",
+                              "text": "฿10",
+                              "size": "sm",
+                              "color": "#111111",
+                              "align": "end"
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "ค่าจัดส่ง",
+                              "size": "sm",
+                              "color": "#555555"
+                            },
+                            {
+                              "type": "text",
+                              "text": "฿0",
+                              "size": "sm",
+                              "color": "#111111",
+                              "align": "end"
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "ทั้งหมด",
+                              "size": "sm",
+                              "color": "#555555"
+                            },
+                            {
+                              "type": "text",
+                              "text": "฿10",
+                              "size": "sm",
+                              "color": "#111111",
+                              "align": "end"
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      "type": "separator",
+                      "margin": "xxl"
+                    },
+                    {
+                      "type": "box",
+                      "layout": "horizontal",
+                      "margin": "md",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "การจ้ายเงิน",
+                          "size": "xs",
+                          "color": "#aaaaaa",
+                          "flex": 0
+                        },
+                        {
+                          "type": "text",
+                          "text": "ปลายทาง",
+                          "color": "#aaaaaa",
+                          "size": "xs",
+                          "align": "end"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                "styles": {
+                  "footer": {
+                    "separator": true
+                  }
+                }
+              }
+        }
+    ]
+}';
+
+var_dump(json_decode($jsonobj, true));
+?>
