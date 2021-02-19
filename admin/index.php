ee3cbb0a477923691f6a68736a0564b1f2925a8a
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-19 21:07:19 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -106,6 +106,7 @@
     </div>
     <!-- /.sidebar -->
   </aside>
+  </nav>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
