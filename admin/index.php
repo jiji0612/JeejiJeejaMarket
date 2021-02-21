51ca2c9b3d8e2218987497e3adf418d03a0bc1dc
JokerSoftYs <36654468+jiji0612@users.noreply.github.com>
2021-02-21 14:41:26 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -36,28 +36,8 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
     <div class="nav navbar navbar-expand-lg navbar-white navbar-light border-bottom p-0">
-      <div class="nav-item dropdown">
-        <a class="nav-link bg-danger dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Close</a>
-        <div class="dropdown-menu mt-0">
-          <a class="dropdown-item" href="#" data-widget="iframe-close" data-type="all">Close All</a>
-          <a class="dropdown-item" href="#" data-widget="iframe-close" data-type="all-other">Close All Other</a>
-        </div>
-      </div>
-      <a class="nav-link bg-light" href="#" data-widget="iframe-scrollleft"><i class="fas fa-angle-double-left"></i></a>
-      <ul class="navbar-nav" role="tablist"></ul>
-      <a class="nav-link bg-light" href="#" data-widget="iframe-scrollright"><i class="fas fa-angle-double-right"></i></a>
-      <a class="nav-link bg-light" href="#" data-widget="iframe-fullscreen"><i class="fas fa-expand"></i></a>
-    </div>
-    <div class="tab-content">
-      <div class="tab-empty">
-        <h2 class="display-4">No tab selected!</h2>
-      </div>
-      <div class="tab-loading">
-        <div>
-          <h2 class="display-4">Tab is loading <i class="fa fa-sync fa-spin"></i></h2>
-        </div>
-      </div>
-    </div>
+      <iframe src="dashboard1.html" style="border: 0; width: 100%; height: 100%">Your browser doesn't support iFrames.</iframe>
+   </div>
   </div>
 
   <!-- /. MFooter -->
